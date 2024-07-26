97d894900f27b6bdf5894b79619b0d8d9dfee2ea
Dio <dionatsir@gmail.com>
2024-07-26 16:11:50 +0700

    add loading login

--- src/components/auth/SignInForm.jsx
+++ src/components/auth/SignInForm.jsx
@@ -4,7 +4,7 @@ import { useRouter, useSearchParams } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import {
   Form,
   FormControl,
@@ -15,6 +15,8 @@ import {
 } from '../ui/form'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import { useState } from 'react'
+import { Loader2 } from 'lucide-react'
 
 const formSchema = z.object({
   username: z.string().min(3),
@@ -25,6 +27,8 @@ export default function SignInForm() {
   const router = useRouter()
   const searchParams = useSearchParams()
 
+  const [loading, setLoading] = useState(false)
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -34,17 +38,20 @@ export default function SignInForm() {
   })
 
   const formSubmit = async (values) => {
+    setLoading(true)
     const login = await signIn('credentials', {
       username: form.getValues('username'),
       password: form.getValues('password'),
       redirect: false,
       callbackUrl: searchParams.get('callbackUrl') || '/dashboard',
     })
+
     if (login?.error) {
       form.setError('username', {
         type: 'manual',
         message: login.error,
       })
+      setLoading(false)
     } else {
       router.refresh()
       router.push(login.url)
@@ -94,9 +101,10 @@ export default function SignInForm() {
           )}
         />
 
-        <div className="flex justify-end pt-4">
-          <Button type="submit" className="sm:w-48 w-full">
-            Submit
+        <div className="flex justify-end items-center pt-4 gap-2">
+          {loading && <Loader2 className="animate-spin" size={26} />}
+          <Button type="submit" className="sm:w-48 w-full" disabled={loading}>
+            Sign In
           </Button>
         </div>
       </form>
